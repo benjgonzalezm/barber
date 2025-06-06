c0ae7de50fa56b1a2decbd3d29fb29ad67ee44d6
antonioDXD7 <101341778+antonioDXD7@users.noreply.github.com>
2025-06-06 00:10:36 -0400

    css y js listos(faltan las redirecciones de los html y las imagenes)

--- barber_base3/gestion3/urls.py
+++ barber_base3/gestion3/urls.py
@@ -4,5 +4,17 @@ from gestion3 import views
 from .views import *
 
 urlpatterns = [
-    path('', views.menu),
-    ]
+    path('', views.menu, name='menu'),
+    path('bloquear/', views.bloquear_usuario, name='bloquear'),
+    path('citas/', views.citas, name='citas'),
+    path('login/', views.logueate, name='login'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('perfil/', views.perfil_view, name='perfil'),
+    path('registrate/', views.registrate, name='registrate'),
+    path('registro-pagos/', views.registro_pagos, name='registro_pagos'),
+    path('registro-barbero/', views.registro_barbero, name='registro_barbero'),
+    path('selecciona-barber/', views.selecciona_barber, name='selecciona_barber'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('testimonios/', views.testimonios, name='testimonios'),
+    path('ver-pagos/', views.ver_pagos, name='ver_pagos'),
+]
--- barber_base3/gestion3/views.py
+++ barber_base3/gestion3/views.py
@@ -2,7 +2,43 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect, render
 
 
-# Create your views here.
 
 def menu(request):
     return render(request, 'gestion3/menu.html')
+
+
+def bloquear_usuario(request):
+    return render(request, 'gestion3/bloquear_usuario.html')
+
+def citas(request):
+    return render(request, 'gestion3/citas.html')
+
+def logueate(request):
+    return render(request, 'gestion3/logueate.html')
+
+def nosotros(request):
+    return render(request, 'gestion3/nosotros.html')
+
+def perfil_view(request):
+    return render(request, 'gestion3/perfil_atenciones.html')
+
+def registrate(request):
+    return render(request, 'gestion3/registrate.html')
+
+def registro_pagos(request):
+    return render(request, 'gestion3/registro_pagos.html')
+
+def registro_barbero(request):
+    return render(request, 'gestion3/registrobarbero.html')
+
+def selecciona_barber(request):
+    return render(request, 'gestion3/selecciona-barber.html')
+
+def servicios(request):
+    return render(request, 'gestion3/servicios.html')
+
+def testimonios(request):
+    return render(request, 'gestion3/testimonios.html')
+
+def ver_pagos(request):
+    return render(request, 'gestion3/ver_pagos.html')
